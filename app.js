0d16372ee27e26bccbf5c091129f1a99e759501d
glitchgirl <londonspleen@gmail.com>
2020-09-02 15:40:23 -0400

    code

---
+++ app.js
@@ -0,0 +1,23 @@
+var unirest = require("unirest");
+
+var req = unirest("GET", "http://api.openweathermap.org/data/2.5/weather");
+
+req.query({
+    "q": "London",
+	"unit": "imperial",
+    "mode": "html",
+    "appid": "INSERT KEY"
+});
+
+req.headers({
+	"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+	"x-rapidapi-key": "INSERT KEY",
+	"useQueryString": true
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
